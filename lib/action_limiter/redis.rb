be4ca38092dc576505e022fe8fe63d8b14fe674f
Maddie Schipper <me@maddiesch.com>
2021-06-02 23:35:37 -0500

    Allow custom Redis connection provider

--- lib/action_limiter/redis.rb
+++ lib/action_limiter/redis.rb
@@ -11,7 +11,20 @@ module ActionLimiter
   #
   # @private
   def self.connection_pool
-    @connection_pool ||= ConnectionPool.new(size: ENV.fetch('ACTION_LIMITER_POOL_SIZE', 5).to_i) do
+    @connection_pool ||= create_connection_pool
+  end
+
+  def self.connection_provider=(connection_provider)
+    @connection_provider = connection_provider
+  end
+
+  ##
+  # @private
+  def self.create_connection_pool
+    pool_size = ENV.fetch('ACTION_LIMITER_POOL_SIZE', 5).to_i
+    timeout = ENV.fetch('ACTION_LIMITER_TIMEOUT', 30).to_i
+
+    ConnectionPool.new(size: pool_size, timeout: timeout) do
       ActionLimiter.create_redis_connection
     end
   end
@@ -21,10 +34,14 @@ module ActionLimiter
   #
   # @private
   def self.create_redis_connection
-    Redis.new(
-      host: ENV.fetch('ACTION_LIMITER_REDIS_HOST', '127.0.0.1'),
-      port: ENV.fetch('ACTION_LIMITER_REDIS_PORT', 6379).to_i,
-      db: ENV.fetch('ACTION_LIMITER_REDIS_DB', 0).to_i
-    )
+    if @connection_provider.respond_to?(:call)
+      @connection_provider.call
+    else
+      Redis.new(
+        host: ENV.fetch('ACTION_LIMITER_REDIS_HOST', '127.0.0.1'),
+        port: ENV.fetch('ACTION_LIMITER_REDIS_PORT', 6379).to_i,
+        db: ENV.fetch('ACTION_LIMITER_REDIS_DB', 0).to_i
+      )
+    end
   end
 end
