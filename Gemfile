22e1eb16059f0c8b3c0c09b167f85348c8af5382
Maddie Schipper <me@maddiesch.com>
2023-03-10 13:43:38 -0600

    Update Ruby in workflow tests

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 gemspec
 
+gem 'pry', '~> 0.14.2'
 gem 'rake', '~> 13.0'
 gem 'rspec', '~> 3.0'
 gem 'rubocop', '~> 1.7'
--- lib/action_limiter/rails.rb
+++ lib/action_limiter/rails.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-require 'action_limiter/rails/engine' if defined?(::Rails)
+require 'action_limiter/rails/engine' if defined?(Rails)
